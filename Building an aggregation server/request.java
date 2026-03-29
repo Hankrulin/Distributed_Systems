2ec015c3b881147787de73611a1a7bbd97de1fe6
Hank <161260307+Hankrulin@users.noreply.github.com>
2026-03-29 12:56:58 +1030

    Delete Building an aggregation server directory

--- Building an aggregation server/request.java
+++
@@ -1,5 +0,0 @@
-public class request {
-    RecipeRequest req = new RecipeRequest(user, ings);
-    String json = om.writerWithDefaultPrettyPrinter().writeValueAsString(req);
-    System.out.println(json);
-}
